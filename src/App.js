f896efbf9c01bcadd3ecab2e7c2ef0513b92d17d
Rafael Melo <rafaelmelo007@gmail.com>
2021-11-14 16:57:05 -0300

    Preventing unmount warnings.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ const PARAM_PAGE = "page="
 const PARAM_HPP = "hitsPerPage="
 
 class App extends Component {
+  _isMounted = false
+
   constructor(props) {
     super(props)
 
@@ -27,9 +29,14 @@ class App extends Component {
   }
 
   componentDidMount = () => {
+    this._isMounted = true
     const { searchTerm } = this.state
     this.setState({ searchKey: searchTerm })
     this.fetchSearchTopStories(searchTerm)
+  }
+
+  componentWillUnmount = () => {
+    this._isMounted = false
   }
 
   onSearchChange = (ev) => {
@@ -116,15 +123,13 @@ class App extends Component {
   }
 
   fetchSearchTopStories = (searchTerm, page = 0) => {
-    const { searchKey, results } = this.state
     axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
-      .then((result) => this.setSearchTopStories(result.data))
-      .catch((error) => {
-        debugger
-        this.setState({ error })
-      })
+      .then(
+        (result) => this._isMounted && this.setSearchTopStories(result.data)
+      )
+      .catch((error) => this._isMounted && this.setState({ error }))
   }
 }
 
