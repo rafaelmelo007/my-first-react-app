8e4f396819fdd81f0a38f9ed27afc9c1dbbf0fc0
Rafael Melo <rafaelmelo007@gmail.com>
2021-11-14 08:31:40 -0300

    Added initial version

---
+++ src/Search.js
@@ -0,0 +1,8 @@
+const Search = ({ value, onChange, children }) => (
+  <form>
+    {children}
+    <input type="text" value={value} onChange={onChange} />
+  </form>
+)
+
+export default Search
---
+++ src/Table.js
@@ -0,0 +1,34 @@
+import Button from "./Button.js"
+
+const isSearched = (searchTerm) => (item) =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
+const largeColumn = {
+  width: "40%",
+}
+const midColumn = {
+  width: "30%",
+}
+const smallColumn = {
+  width: "10%",
+}
+
+const Table = ({ list, searchTerm, onDismiss }) => (
+  <div className="table">
+    {list.filter(isSearched(searchTerm)).map((item) => (
+      <div key={item.objectID} className="table-row">
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
+          <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+        </span>
+      </div>
+    ))}
+  </div>
+)
+
+export default Table
